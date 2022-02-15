4345e72e4fcf205d6bd1e50bab012d5c26ef2a82
sivasuryanarayananv <sivasuryanarayanan.v@syncfusion.com>
2022-02-15 22:22:23 +0530

    KB sample included
    KB sample included

---
+++ MapsGettingStarted/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Windows.Forms.Maps;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapsGettingStarted
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShapeFileLayer shapeLayer = new ShapeFileLayer();
+
+            shapeLayer.Uri = "world1.shp";
+
+            this.mapsControl1.Layers.Add(shapeLayer);
+
+        }
+    }
+}
+
